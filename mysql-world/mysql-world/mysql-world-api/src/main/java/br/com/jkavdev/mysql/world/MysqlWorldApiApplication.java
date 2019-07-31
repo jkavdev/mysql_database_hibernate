c7e211bb739245b0d12f8a5a1b5709cce0ed04a1
jkavdev <jhonatankolen@gmail.com>
2019-07-31 00:48:22 -0300

    adicionando projeto spring boot api world

---
+++ mysql-world/mysql-world/mysql-world-api/src/main/java/br/com/jkavdev/mysql/world/MysqlWorldApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.jkavdev.mysql.world;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlWorldApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlWorldApiApplication.class, args);
+	}
+
+}
